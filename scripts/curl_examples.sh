849e79eef635cb97d95a17d797034ae5f7b9c1f1
Dwayne Yuen <dwayneyuen@gmail.com>
2024-02-12 12:18:45 -0800

    fix: document examples

--- scripts/curl_examples.sh
+++ scripts/curl_examples.sh
@@ -1,6 +1,12 @@
+# Create an address
 curl -XPOST localhost:3000/addresses -d '{"name": "Whole Foods", "street": "100 Van Ness Avenue", "city": "San Francisco"}'
+
+# Get addresses
 curl -XGET localhost:3000/addresses
 
+# Create a package
 curl -XPOST localhost:3000/packages -d '{"name": "Apples", "weight": 100, "addressId": 1}'
+
+# Get packages
 curl -XGET localhost:3000/packages
 
